40d877690d8b6c6eb63cb30d343f3120d2fec085
thisdotLolu <davidebe63@gmail.com>
2023-11-21 12:41:04 +0100

    feat: add date fns

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -4,7 +4,7 @@ import {timestamp} from '../../firebase/config'
 import {useAuthContext} from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore'
 import Avatar from '../../components/Avatar'
-
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 
 const ProjectComments = ({project}) => {
@@ -45,7 +45,9 @@ const ProjectComments = ({project}) => {
                         <p>{comment.displayName}</p>
                     </div>
                     <div className='comment-date'>
-                        <p>date here</p>
+                        <p>{formatDistanceToNow(comment.createdAt.toDate(), {
+                            addSuffix:true
+                        })}</p>
                     </div>
                     <div className='comment-content'>
                         <p>{comment.content}</p>
